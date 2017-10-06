217ba3cc5a863aff2e016c7d733331ebe8add5a3
Thiago Capelo <thiagokpelo@gmail.com>
2017-10-05 23:53:55 -0300

    Adicionando Javascript no projeto

--- src/js/app.js
+++ src/js/app.js
@@ -2,28 +2,99 @@
 
     'use strict';
 
+    var MODEL = {
+
+        init: function() {},
+
+        tasks: [
+            { id: 1, title: 'Work it harder', description: 'Lorem ipsum fa fdasj fasij' },
+            { id: 2, title: 'Make it better!', description: 'Lorem ipsum fa fdasj fasij' },
+            { id: 3, title: 'Testando essa parada', description: '' }
+        ]
+     };
+
+    var CONTROLLER = {
+
+        init: function( args ) {
+            console.log( args );
+
+            MODEL.init();
+            VIEW.init();
+        },
+
+        listTasks: function() {
+            return MODEL.tasks;
+        },
+
+        getTask: function( id ) {
+            return MODEL.tasks.filter(function( t ) {
+                return id == t.id;
+            });
+        }
+    };
+
     var VIEW = {
         init: function() {
+            VIEW.$taskDetail.hide();
 
-            console.log('VIEW INIT');
+            VIEW.listTasks().map( VIEW.createTasksList );
 
-            var $taskDetail      = document.querySelector( '.task-detail' );
-            var $buttonAddTask   = document.querySelector( '#addTask' );
-            var $buttonCloseTask = document.querySelector( '#closeTask' );
-
-            $buttonAddTask.addEventListener( 'click', function() {
-                $taskDetail.show()
+            VIEW.$buttonAddTask.addEventListener( 'click', function() {
+                VIEW.$taskDetail.show();
             });
-            $buttonCloseTask.addEventListener( 'click', function() {
-                $taskDetail.hide();
+
+            VIEW.$buttonCloseTask.addEventListener( 'click', function() {
+                VIEW.$taskDetail.hide();
             });
+
+            VIEW.$body.addEventListener( 'click', function( event ) {
+                if (event.target.tagName.toLowerCase() === 'span') {
+                    VIEW.editTask( event.target.dataset.id );
+                }
+            });
+        },
+
+        $body:            document.querySelector( 'body' ),
+        $taskDetail:      document.querySelector( '.task-detail' ),
+        $tasksList:       document.querySelector( '#tasksList' ),
+        $tasks:           document.querySelectorAll( '#tasksList span' ),
+        $buttonAddTask:   document.querySelector( '#addTask' ),
+        $buttonCloseTask: document.querySelector( '#closeTask' ),
+
+        createTasksList: function( task ) {
+            var $li       = document.createElement( 'li' );
+            var $span     = document.createElement( 'span' );
+            var title     = document.createTextNode( task.title );
+            var attrData  = document.createAttribute( 'data-id' );
+
+            attrData.value = task.id;
+            $span.setAttributeNode( attrData );
+            $span.appendChild( title );
+            $li.appendChild( $span );
+            VIEW.$tasksList.appendChild( $li );
+        },
+
+        listTasks: function() {
+            return CONTROLLER.listTasks();
+        },
+
+        editTask: function( id ) {
+            var task = CONTROLLER.getTask( id )[0];
+
+            document.querySelector( '#titleTaksDetail' ).value = task.title;
+            document.querySelector( '#descriptionTaskDetail' ).value = task.description;
+
+            VIEW.$taskDetail.show();
+        },
+
+        populateTask: function() {
 
         },
     };
 
-    VIEW.init();
-
-
+    document.addEventListener( 'DOMContentLoaded', function() {
+        CONTROLLER.init( 'Init App' );
+    });
 
 })();
 
